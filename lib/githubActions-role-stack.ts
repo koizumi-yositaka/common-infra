1e9971b93c0c5f2af14b00ab2d223338075383a7
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2025-09-11 01:01:51 +0900

    repository情報を更新

--- lib/githubActions-role-stack.ts
+++ lib/githubActions-role-stack.ts
@@ -3,26 +3,28 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 const GITHUB_USERNAME = "koizumi-yositaka"
-const REPOSITORY_NAME = "manage-work-lambda"
+const REPOSITORY_NAME_LIST = ["manage-work-lambda","common-infra"]
 
 export class GitHubActionsRoleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-// GitHubのOIDCプロバイダーを作成
+    // GitHubのOIDCプロバイダーを作成
     const githubProvider = new iam.OpenIdConnectProvider(this, 'GitHubProvider', {
       url: 'https://token.actions.githubusercontent.com',
       clientIds: ['sts.amazonaws.com'],
     });
 
-// github actions からの使用を許可したIAMロールを作成
+    // github actions からの使用を許可したIAMロールを作成
+    const stringLike = REPOSITORY_NAME_LIST.map(x => `repo:${GITHUB_USERNAME}/${x}:*`);
+    console.log(stringLike)
     const githubActionsRole = new iam.Role(this, 'GitHubActionsRole', {
       assumedBy: new iam.WebIdentityPrincipal(githubProvider.openIdConnectProviderArn, {
         StringEquals: {
           'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com',
         },
         StringLike: {
-          'token.actions.githubusercontent.com:sub': `repo:${GITHUB_USERNAME}/${REPOSITORY_NAME}:*`
+          'token.actions.githubusercontent.com:sub': stringLike
         }
       }),
       description: 'Role for GitHub Actions to deploy CDK stack',
